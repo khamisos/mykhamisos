d8eb4e1fc8f7ac0a22fec832de1bb5d53b917c79
Khamisos <63805504+khamisos@users.noreply.github.com>
2024-06-13 12:33:07 +0400

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from transformers import AutoModelForSeq2SeqLM, AutoTokenizer
 
 @st.cache_resource
 def load_model():
-    model_name = "sshleifer/distilbart-cnn-12-6"
+    model_name = "moussaKam/arabert"
     tokenizer = AutoTokenizer.from_pretrained(model_name)
     model = AutoModelForSeq2SeqLM.from_pretrained(model_name)
     return tokenizer, model
