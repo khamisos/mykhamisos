e70561cdb808ae345494aa13fcae31fea05db26d
Khamisos <63805504+khamisos@users.noreply.github.com>
2024-06-13 12:03:37 +0400

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,11 @@
 import streamlit as st
-from transformers import MBartForConditionalGeneration, MBart50TokenizerFast
+from transformers import AutoModelForSeq2SeqLM, AutoTokenizer
 
 @st.cache_resource
 def load_model():
-    model_name = "facebook/mbart-large-50-many-to-many-mmt"
-    tokenizer = MBart50TokenizerFast.from_pretrained(model_name)
-    model = MBartForConditionalGeneration.from_pretrained(model_name)
-    tokenizer.src_lang = "ar_AR"
-    tokenizer.tgt_lang = "ar_AR"
+    model_name = "akhooli/arabic-bart-small"
+    tokenizer = AutoTokenizer.from_pretrained(model_name)
+    model = AutoModelForSeq2SeqLM.from_pretrained(model_name)
     return tokenizer, model
 
 def summarize(text, tokenizer, model):
@@ -18,8 +16,7 @@ def summarize(text, tokenizer, model):
         min_length=25,
         length_penalty=2.0,
         num_beams=4,
-        early_stopping=True,
-        forced_bos_token_id=tokenizer.lang_code_to_id["ar_AR"]
+        early_stopping=True
     )
     summary = tokenizer.decode(summary_ids[0], skip_special_tokens=True)
     return summary
